4b69a79c7b1a46ad6dff884334e305d96173b1fa
Caleb Byrd <cbyrd@strategicmarketing.com>
2018-03-21 11:00:49 -0400

    as

--- RestAPItest/Controllers/HomeController.cs
+++ RestAPItest/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace RestAPItest.Controllers
         private APIClientConsumer _api = new APIClientConsumer();
         public async Task<ActionResult> Index()
         {
-            //testing source contrl
+            //testing source cntrl
             await _api.GetUsers();
             return View();
         }
